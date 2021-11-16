24307ba63ac6d3a2cf4df67a702ccb42c7050e28
Pranav Chinni <94307417+tornadofury111@users.noreply.github.com>
2021-11-16 02:20:30 +0000

    Returns the data sent

--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 import requests
 params = {'id': 'c', 'temperature':'v','humidity':'s'}
 response = requests.get('https://flask-innovation-project-1.pranavchinni.repl.co/',params=params)
-print(response.url)
+print(response.text)
